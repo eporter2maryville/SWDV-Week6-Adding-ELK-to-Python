6ae76ea7e5e1e018e788230ee6834b848e4edeb6
eporter2maryville <39113621+eporter2maryville@users.noreply.github.com>
2020-02-22 11:59:30 -0700

    Added LogStash

--- index.py
+++ index.py
@@ -3,6 +3,30 @@
 #Adding ELK to a Python Application
 
 #Placeholder for import
+import logging
+import logstash
+import sys
+import time
+
+#creating logger
+pythonELKLogger = logging.getLogger('python-logstash-logger')
+pythonELKLogger.setLevel(logging.INFO)
+pythonELKLogger.addHandler(logstash.LogstashHandler('34.201.47.23', 5959, version=1))
+
+pythonELKLogger.error('python-logstash: test logstash error message.')
+pythonELKLogger.info('python-logstash: test logstash info message.')
+pythonELKLogger.warning('python-logstash: test logstash warning message.')
+
+#Add extra field to logstash
+extra = {
+    'test_string': 'python version: ' + repr(sys.version_info),
+    'test_boolean': True,
+    'test_dict': {'a': 1, 'b': 'c'},
+    'test_float': 1.23,
+    'test_integer': 126,
+    'test_list': [1,2,3],
+}
+pythonELKLogger.info('python-logstash: test extra fields', extra=extra)
 
 #Input class
 class whoAreYou:
